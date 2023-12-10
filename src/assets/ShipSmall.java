b0be18984cf72ff618e61ce93b542ef6887b128f
CH7733 <[louis.schadewaldt@freent.de]>
2023-12-10 13:49:55 +0100

    Changes by Jonas

--- src/assets/ShipSmall.java
+++ src/assets/ShipSmall.java
@@ -1,5 +1,7 @@
 package assets;
 
+import java.util.ArrayList;
+
 import static programm.Manager.getCounterShips;
 
 /**
@@ -17,9 +19,10 @@ public class ShipSmall extends Ship{
     }
 
     /**
-     * Konstruktor für ein neues kleines Schiff mit einer spezifischen ID.
+     * Alternativer Konstruktor für ein kleines Schiff, der "leere Schiffe" erstellt.
      *
-     * @param id Die ID des kleinen Schiffs (negativer Wert als Platzhalter)
+     * @param id Die eindeutige ID des großen Schiffs
+     *            (Negative IDs dienen hier als theoretische Platzhalter und sollten nicht in der Praxis verwendet werden.)
      */
     // Die negative ID stellt klar, dass das nur ein theoretischer Platzhalter ist
     public ShipSmall(int id){
